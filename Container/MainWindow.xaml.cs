5c4573fbbb3c48240075e74850b919cc51357908
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-11-03 12:30:31 +0100

    Tag2: Container, Layouting, Events

--- Container/MainWindow.xaml.cs
+++ Container/MainWindow.xaml.cs
@@ -27,7 +27,8 @@ namespace Container
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Canvas.SetLeft(Rct_Blue, 200);
+            //Codeseitiger Schreib-Zugriff auf eine Attached-Property des Canvas
+            Canvas.SetLeft(Rct_Blue, 50);
         }
     }
 }
