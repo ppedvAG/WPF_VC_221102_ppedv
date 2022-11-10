f40f3c0260f18a586931c62a7ac971cad781cd57
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-11-10 12:38:55 +0100

    Tag 7: RelativeSources, Lokalisierung, Commands

---
+++ Commands/CloseCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Commands
+{
+    //Auf eine Aufgabe (das Schließen eines Fensters) spezialisierte Command-Klasse (vgl. CustomCommand.cs)
+    public class CloseCommand : ICommand
+    {
+        //Anmeldung des Events im CommandManager der Applikation
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        //Bedingung für Ausführung
+        public bool CanExecute(object? parameter)
+        {
+            return (parameter as MainWindow).Width < 150;
+        }
+
+        //Aktion bei Ausführung
+        public void Execute(object? parameter)
+        {
+            (parameter as MainWindow).Close();
+        }
+    }
+}
---
+++ Commands/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Commands
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Initialisierung der Commands
+            CloseCmd = new CloseCommand();
+            OeffnenCmd = new CustomCommand
+                (
+                    //Übergabe der Execute()-Logik
+                    p => (new MainWindow()).Show(),
+                    //Übergabe der CanExecute()-Logik
+                    p => (p as string).Length >= 1
+                );
+
+            //Setzen des DataContext
+            this.DataContext = this;
+        }
+
+        //Commandproperties 
+        public CloseCommand CloseCmd { get; set; }
+        public CustomCommand OeffnenCmd { get; set; }
+    }
+}
